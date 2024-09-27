1c321543bc52af919498c36ab477e8e0d01e6837
Eduard van Valkenburg <eavanvalkenburg@users.noreply.github.com>
2024-09-27 23:13:35 +0200

    Python: reordered the data folder (#8910)
    ### Motivation and Context
    
    <!-- Thank you for your contribution to the semantic-kernel repo!
    Please help reviewers and future users, providing the following
    information:
      1. Why is this change required?
      2. What problem does it solve?
      3. What scenario does it contribute to?
      4. If it fixes an open issue, please link to the issue here.
    -->
    
    Reordered the data folder
    Removed NOT_EQUAL_TO filter clause
    Changed search functions to search, get_search_result and
    get_text_search_result for TextSearch and vectorizable_text_search and
    vectorized_search for VectorSearch to reflect updates to dotnet.
    Removed other query types for VectorSearch
    Added exceptions and handling of no result error.
    
    ### Description
    
    <!-- Describe your changes, the overall approach, the underlying design.
    These notes will help understanding how your code works. Thanks! -->
    
    ### Contribution Checklist
    
    <!-- Before submitting this PR, please make sure: -->
    
    - [x] The code builds clean without any errors or warnings
    - [x] The PR follows the [SK Contribution
    Guidelines](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md)
    and the [pre-submission formatting
    script](https://github.com/microsoft/semantic-kernel/blob/main/CONTRIBUTING.md#development-scripts)
    raises no violations
    - [ ] All unit tests pass, and I have added new tests where possible
    - [x] I didn't break anyone :smile:
    
    ---------
    
    Co-authored-by: Tao Chen <taochen@microsoft.com>

--- python/semantic_kernel/data/const.py
+++ python/semantic_kernel/data/const.py
@@ -1,8 +1,12 @@
 # Copyright (c) Microsoft. All rights reserved.
 
-
 from enum import Enum
 from typing import Final
+
+DEFAULT_DESCRIPTION: Final[str] = (
+    "Perform a search for content related to the specified query and return string results"
+)
+DEFAULT_COUNT: Final[int] = 5
 
 
 class IndexKind(str, Enum):
@@ -19,28 +23,3 @@ class DistanceFunction(str, Enum):
     DOT_PROD = "dot_prod"
     EUCLIDEAN = "euclidean"
     MANHATTAN = "manhattan"
-
-
-class VectorSearchQueryTypes(str, Enum):
-    """Types of vector search queries.
-
-    Attributes:
-        VECTORIZED_SEARCH_QUERY: A type of query that searches a vector store using a vector.
-        VECTORIZABLE_TEXT_SEARCH_QUERY: A type of query that search a vector store using a
-            text string that will be vectorized downstream.
-        HYBRID_TEXT_VECTORIZED_SEARCH_QUERY: A type of query that does a hybrid search in
-            a vector store using a vector and text.
-        HYBRID_VECTORIZABLE_TEXT_SEARCH_QUERY: A type of query that does a hybrid search
-            using a text string that will be vectorized downstream.
-    """
-
-    VECTORIZED_SEARCH_QUERY = "vectorized_search_query"
-    VECTORIZABLE_TEXT_SEARCH_QUERY = "vectorizable_text_search_query"
-    HYBRID_TEXT_VECTORIZED_SEARCH_QUERY = "hybrid_text_vectorized_search_query"
-    HYBRID_VECTORIZABLE_TEXT_SEARCH_QUERY = "hybrid_vectorizable_text_search_query"
-
-
-DEFAULT_DESCRIPTION: Final[str] = (
-    "Perform a search for content related to the specified query and return string results"
-)
-DEFAULT_COUNT: Final[int] = 5
